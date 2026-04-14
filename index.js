83ed7da746de61c7be8a651dca02067a5cff6114
MRili123 <iliasssjb2004@gmail.com>
2026-04-14 09:49:26 +0100

    setup the counter project

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const FILE = path.join(__dirname, "visits.json");
+
+function readCounter() {
+  if (!fs.existsSync(FILE)) {
+    fs.writeFileSync(FILE, JSON.stringify({ count: 0 }));
+  }
+  const data = fs.readFileSync(FILE);
+  return JSON.parse(data).count;
+}
+
+function writeCounter(count) {
+  fs.writeFileSync(FILE, JSON.stringify({ count }));
+}
+
+app.get("/", (req, res) => {
+  let count = readCounter();
+  count++;
+  writeCounter(count);
+
+  res.send(`App de Ilias  - Nombre de visites : ${count}`);
+});
+
+app.listen(PORT, () => {
+  console.log("Server running...");
+});
